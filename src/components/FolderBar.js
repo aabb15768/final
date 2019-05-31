c3ed43ce1fb03d848bde6ecc48eb83cba84c1c11
aabb15768 <b03204032@ntu.edu.tw>
2019-05-31 17:06:56 +0800

    final commit

--- src/components/FolderBar.js
+++ src/components/FolderBar.js
@@ -7,8 +7,8 @@ class FolderBar extends React.Component {
         return(
             <tr>
                 <td className="menu" id="folder-bar" colSpan={this.props.width}>
-                    <div id="folder1"><a href="https://github.com/aabb15768/midterm_minesweeper/blob/master/README.md" target="blank">Read Me </a></div>
-                    <div id="folder2"><a href="https://github.com/aabb15768/midterm_minesweeper/tree/master/src" target="blank">Source Code</a></div>
+                    <div id="folder1"><a href="https://github.com/aabb15768/final/blob/master/README.md" target="blank">Read Me </a></div>
+                    <div id="folder2"><a href="https://github.com/aabb15768/final/tree/master/src" target="blank">Source Code</a></div>
                 </td>
             </tr>
         );
