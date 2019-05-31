7b52a241635b96158db313666edce7c081d5d534
aabb15768 <b03204032@ntu.edu.tw>
2019-05-31 17:02:22 +0800

    Updates

---
+++ precache-manifest.6bb342153012ffef2c40d07d3130cb4a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8e00883961ed56cac980b6be3260b0",
+    "url": "/final/index.html"
+  },
+  {
+    "revision": "9b3c1581b6fad3afc03d",
+    "url": "/final/static/css/main.d6234a94.chunk.css"
+  },
+  {
+    "revision": "ef444661c4dbdf3d0cd1",
+    "url": "/final/static/js/2.9cd5913a.chunk.js"
+  },
+  {
+    "revision": "9b3c1581b6fad3afc03d",
+    "url": "/final/static/js/main.3747d4b7.chunk.js"
+  },
+  {
+    "revision": "8f172ccf02a3401528e8",
+    "url": "/final/static/js/runtime~main.96efe899.js"
+  },
+  {
+    "revision": "582cac299faac1ab8d378a6488ba2ced",
+    "url": "/final/static/media/digital-dismay.582cac29.otf"
+  },
+  {
+    "revision": "2d4abfe08e4c9b19a658317f762edd06",
+    "url": "/final/static/media/easy.2d4abfe0.png"
+  },
+  {
+    "revision": "1517918573e289534a9006f8fb9be6ad",
+    "url": "/final/static/media/flag.15179185.png"
+  },
+  {
+    "revision": "756d2d336ea28cbf6a76246dde04c089",
+    "url": "/final/static/media/hard.756d2d33.png"
+  },
+  {
+    "revision": "6f511e4c71e01b72fced59ad183e759f",
+    "url": "/final/static/media/medium.6f511e4c.png"
+  },
+  {
+    "revision": "e3a9c893cd405aca729270d3cbc49559",
+    "url": "/final/static/media/micross.e3a9c893.ttf"
+  },
+  {
+    "revision": "0006461ae714073d229291f960b74b30",
+    "url": "/final/static/media/mine-menu-icon.0006461a.png"
+  },
+  {
+    "revision": "65cb344c64e3d29ae35396789de64c2e",
+    "url": "/final/static/media/prstart.65cb344c.ttf"
+  },
+  {
+    "revision": "821525386b0319d0bbe53ed70b8bed9a",
+    "url": "/final/static/media/window-controls.82152538.png"
+  }
+]);
